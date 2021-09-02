2a0309051acdf714d11cbe72b56d227d60fa4111
Nikolay <k0jl9ih@yandex.ru>
2021-09-02 23:43:31 +0300

    static files fixed

--- conf/settings.py
+++ conf/settings.py
@@ -41,6 +41,7 @@ INTERNAL_IPS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
